fe417d1517b51ab20f819799b62db22c6caa5ea3
dsha256 <shainidzedavid@gmail.com>
2022-04-11 18:05:46 +0400

    feat: add PASETO implementation of the Maker interface and test it

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,62 @@
+package token
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewJwtMaker(RandomString(32))
+	require.NoError(t, err)
+
+	username := RandomString(32)
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, payload, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(RandomString(32))
+	require.NoError(t, err)
+
+	token, payload, err := maker.CreateToken(RandomString(32), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(RandomString(32))
+	require.NoError(t, err)
+
+	token, payload, err := maker.CreateToken(RandomString(32), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token[1:])
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+}
